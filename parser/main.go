a61f6b9daf2147f4b23506f66c91284e9fa4bf11
stu-k <stuart.kilgore@gmail.com>
2025-04-06 12:24:02 -0500

    abstract types

--- parser/main.go
+++ parser/main.go
@@ -4,30 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"unicode"
 
-	"github.com/stu-k/go/parser/errors"
+	"github.com/stu-k/go/parser/parse"
 )
 
 type Data interface {
 	Type() string
 	Value() any
 	String() string
-}
-
-type ParseChecker interface {
-	Parse(s string) (Data, string, error)
-	Check(r rune) bool
-}
-
-var mainOpts = []ParseChecker{
-	&Var{},
-	&Num{},
-	&Obj{},
-	&Arr{},
-	&Str{},
-	&Paren{},
-	&Op{},
 }
 
 func main() {
@@ -40,7 +24,7 @@ func main() {
 		input := scanner.Text()
 
 		fmt.Printf("parsing: \"%v\"\n", input)
-		result, err := mainParse(input)
+		result, err := parse.Parse(input)
 		if err != nil {
 			fmt.Printf("error parsing input: %v\n", err)
 			continue
@@ -48,77 +32,4 @@ func main() {
 
 		fmt.Printf("result: %+v\n", result)
 	}
-}
-
-func mainParse(input string) ([]Data, error) {
-	data := make([]Data, 0)
-
-	result, rest, err := parse(input, mainOpts)
-	if err != nil {
-		return nil, err
-	}
-	data = append(data, result)
-
-	for rest != "" {
-		result, rest, err = parse(rest, mainOpts)
-		if err != nil {
-			return nil, err
-		}
-		data = append(data, result)
-	}
-
-	return data, nil
-}
-
-func parse(input string, opts []ParseChecker) (Data, string, error) {
-	fmt.Printf("\nparse: \"%v\"\n", input)
-	if len(input) == 0 {
-		return nil, "", errors.NewEndOfInputErr()
-	}
-
-	r := rune(input[0])
-	if unicode.IsSpace(r) {
-		return parse(input[1:], opts)
-	}
-
-	type result struct {
-		data Data
-		rest string
-		err  error
-	}
-
-	var okResults []result
-	var errResults []result
-	for _, opt := range mainOpts {
-		ok := opt.Check(r)
-		if !ok {
-			continue
-		}
-
-		res, rest, err := opt.Parse(input)
-		if err != nil {
-			errResults = append(okResults, result{
-				res, rest, err,
-			})
-			continue
-		}
-		okResults = append(okResults, result{
-			res, rest, err,
-		})
-	}
-
-	fmt.Printf("ok results: %v\n", okResults)
-	fmt.Printf("err results: %v\n", errResults)
-
-	if len(okResults) > 0 {
-		r := okResults[0]
-		return r.data, r.rest, r.err
-	}
-
-	if len(errResults) > 0 {
-		r := errResults[0]
-		return r.data, r.rest, r.err
-	}
-
-	return nil, "", errors.NewUnexpectedCharErr(r)
 }
