1d80d417eccd10d1d575e0cc2f8c54ce05c968a8
stu-k <stuart.kilgore@gmail.com>
2025-04-10 19:42:59 -0500

    cleanup rule str parsing

--- parser/syntax/rule.go
+++ parser/syntax/rule.go
@@ -1,7 +1,6 @@
 package syntax
 
 import (
-	"fmt"
 	"unicode"
 
 	"github.com/stu-k/go/parser/errors"
@@ -13,11 +12,11 @@ var defaultRulemap = map[string]*Rule{
 }
 
 var ruleAny = &Rule{
-	name:      "DEFAULT_ANY_RULE",
-	repeat:    -1,
-	capture:   true,
-	checkChar: func(_ string) bool { return true },
-	modified:  false,
+	name:     "DEFAULT_ANY_RULE",
+	repeat:   -1,
+	capture:  true,
+	check:    func(_ string) bool { return true },
+	modified: false,
 }
 
 var RuleAlpha = NewRule("alpha").CheckChar(unicode.IsLetter)
@@ -39,9 +38,9 @@ type Rule struct {
 	// capture determines if the match should be returned
 	capture bool
 
-	// checkChar is the fn used to validate if the characters
+	// check is the fn used to validate if the characters
 	// in a string are valid for the rule
-	checkChar func(string) bool
+	check func(string) bool
 
 	// modified informs if the rule has been changed from the default
 	modified bool
@@ -56,7 +55,7 @@ func (a *Rule) clone() *Rule {
 		modified: true,
 	}
 	new.repeat = a.repeat
-	new.checkChar = a.checkChar
+	new.check = a.check
 	return new
 }
 
@@ -75,7 +74,7 @@ func (a *Rule) Name() string {
 }
 
 func (a *Rule) CheckChar(fn func(rune) bool) *Rule {
-	a.checkChar = func(s string) bool {
+	a.check = func(s string) bool {
 		return len(s) > 0 && fn(rune(s[0]))
 	}
 	return a
@@ -98,7 +97,7 @@ func (a *Rule) Chars(s string) *Rule {
 		return ok
 	}
 
-	a.checkChar = check
+	a.check = check
 	return a
 }
 
@@ -112,36 +111,7 @@ func (a *Rule) Repeat(n int) *Rule {
 }
 
 func (a *Rule) Parse(s string) (*Result, error) {
-	if a.checkChar == nil {
-		// defaulting to "none" rule to invalidate null pointers
-		fmt.Println("[ERR] DEFAULTED_NONE_RULE")
-		return a.clone().
-			Named("DEFAULTED_NONE_RULE").
-			CheckChar(func(_ rune) bool { return false }).
-			Parse(s)
-	}
-
-	return a.parseChar(s)
-}
-
-func (a *Rule) parseChar(s string) (*Result, error) {
-	return charParser{
-		a.name,
-		a.repeat,
-		a.capture,
-		a.checkChar,
-	}.Parse(s)
-}
-
-type charParser struct {
-	name    string
-	repeat  int
-	capture bool
-	check   func(string) bool
-}
-
-func (p charParser) Parse(s string) (*Result, error) {
-	shouldRepeat := p.repeat > 0
+	shouldRepeat := a.repeat > 0
 
 	var result string
 	var count int
@@ -149,11 +119,11 @@ func (p charParser) Parse(s string) (*Result, error) {
 	for _, c := range s {
 		r := rune(c)
 
-		if shouldRepeat && count >= p.repeat {
+		if shouldRepeat && count >= a.repeat {
 			break
 		}
 
-		ok := p.check(string(r))
+		ok := a.check(string(r))
 		if !ok {
 			break
 		}
@@ -163,17 +133,17 @@ func (p charParser) Parse(s string) (*Result, error) {
 	}
 
 	if len(result) == 0 {
-		return retErr(p.name, errors.NewBadMatchErr(p.name, s, "parsechar:nores"))
+		return retErr(a.name, errors.NewBadMatchErr(a.name, s, "parsechar:nores"))
 	}
 
-	if shouldRepeat && count < p.repeat {
-		return retErr(p.name, errors.NewBadMatchErr(p.name, s, "parsechar:toofew"))
+	if shouldRepeat && count < a.repeat {
+		return retErr(a.name, errors.NewBadMatchErr(a.name, s, "parsechar:toofew"))
 	}
 
-	if !p.capture {
-		return NewResult(p.name, nil, s[count:]), nil
+	if !a.capture {
+		return NewResult(a.name, nil, s[count:]), nil
 	}
 
-	return NewResult(p.name, []string{result}, s[count:]), nil
+	return NewResult(a.name, []string{result}, s[count:]), nil
 
 }
